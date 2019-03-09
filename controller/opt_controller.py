d4d10641dbfc51a172523390695293ce3942f0d1
Sherwin Lee <sherwin.h.lee@gmail.com>
2019-03-08 21:32:12 -0500

    Delete opt_controller.py

--- controller/opt_controller.py
+++
@@ -1,7 +0,0 @@
-if __name__== "__main__":
-    parent = os.path.realpath('..')
-    sys.stdout = open(parent + '/logs/' + logfile + '.txt','w') # PARAMETER
-    obj = AccControl(robot='create1',accel=0.05,vel=None,coordsys='s',delay=10)
-    obj.control()
-    sys.stdout.close()
-    plot(obj.x,obj.y,X_LABLIM,Y_LABLIM,'Robot Trajectory')
